720dc673510929ddaef6302c3051107237ae8ccf
milgo <milgo@o2.pl>
2015-03-27 06:35:13 +0100

    Class expreval

---
+++ expreval.h
@@ -0,0 +1,43 @@
+#ifndef __EXPREVAL
+#define __EXPREVAL
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+enum EXPR_EVAL_ERR {
+	EEE_NO_ERROR = 0,
+	EEE_PARENTHESIS = 1,
+	EEE_WRONG_CHAR = 2,
+	EEE_DIVIDE_BY_ZERO = 3
+};
+
+typedef char EVAL_CHAR;
+
+class ExprEval {
+private:
+	EXPR_EVAL_ERR _err;
+	EVAL_CHAR* _err_pos;
+	int _paren_count;
+
+	// Parse a number or an expression in parenthesis
+	double ParseAtom(EVAL_CHAR*& expr);
+
+	// Parse multiplication and division
+	double ParseFactors(EVAL_CHAR*& expr);
+
+	// Parse addition and subtraction
+	double ParseSummands(EVAL_CHAR*& expr);
+
+public:
+	double Eval(EVAL_CHAR* expr);
+	EXPR_EVAL_ERR GetErr() {
+		return _err;
+	}
+	EVAL_CHAR* GetErrPos() {
+		return _err_pos;
+	}
+};
+
+#endif
+
